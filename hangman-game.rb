15296b059263c966361f9fd065bea0e8b3de4e5b
BerettaSM <ramonsaviato@hotmail.com>
2023-09-29 20:41:50 -0300

    feat: add word filtering

--- hangman-game.rb
+++ hangman-game.rb
@@ -3,13 +3,17 @@ class HangmanGame
 
     def initialize
         read_words
+        
     end
 
     private
 
     def read_words
         file_text = File.read("./google-10000-english-no-swears.txt")
-        @words = file_text
+        @words = filter_words(file_text)
     end
 
+    def filter_words text
+        text.split.filter { |word| word.length >= 5 && word.length <= 12}
+    end
 end
