84304fab618c6542fd31a0220b3c6593a36fc445
BerettaSM <ramonsaviato@hotmail.com>
2023-09-29 21:33:16 -0300

    feat: add main game loop

--- hangman-game.rb
+++ hangman-game.rb
@@ -4,6 +4,7 @@ require "./ascii-art.rb"
 class HangmanGame
     attr_reader :words
     attr_reader :choosen_word
+    attr_reader :word_state
 
     MENU_OPTIONS = {
         "1" => "Start new game",
@@ -17,7 +18,14 @@ class HangmanGame
 
     def start
         print_menu
-        user_input = read_input MENU_OPTIONS.keys, "Invalid menu option."
+        user_input = read_menu_option
+
+        case user_input
+        when "1"
+            main_game_loop
+        when "2"
+            raise NotImplementedError
+        end
     end
 
     private
@@ -33,6 +41,7 @@ class HangmanGame
 
     def pick_word
         @choosen_word = @words.sample(1)[0].downcase
+        @word_state = Array.new(@choosen_word.length) { "_" }
     end
 
     def print_menu
@@ -41,11 +50,27 @@ class HangmanGame
         MENU_OPTIONS.each_pair { |number, text| puts "#{number}. #{text}" }
     end
 
-    def read_input valid_choices, err_message
+    def read_menu_option
         loop do
             choice = gets.chomp
-            return choice unless !valid_choices.include? choice
-            puts err_message
+            return choice unless !MENU_OPTIONS.keys.include? choice
+            puts "Invalid menu option."
         end
+    end
+
+    def read_user_input
+        print "\nEnter a letter: "
+        gets.chomp[0].downcase
+    end
+
+    def main_game_loop
+        pick_word
+        print_word_state
+
+        user_input = read_user_input
+    end
+
+    def print_word_state
+        puts @word_state.join(" ")
     end
 end
