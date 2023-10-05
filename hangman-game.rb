83ac160353508f800397dfb915716c7510697b68
BerettaSM <ramonsaviato@hotmail.com>
2023-10-04 22:40:26 -0300

    feat: completion

--- hangman-game.rb
+++ hangman-game.rb
@@ -15,6 +15,7 @@ class HangmanGame
 
     def initialize
         read_words
+        @user_guesses = 0
         @lives = 5
     end
 
@@ -26,9 +27,19 @@ class HangmanGame
 
         case user_input
         when "1"
+            pick_word
             main_game_loop
         when "2"
-            raise NotImplementedError
+            if File.exist?('./save.txt')
+                loaded_state = Marshal.load(File.read('./save.txt'))
+                @word_state = loaded_state["word_state"]
+                @choosen_word = loaded_state["choosen_word"]
+                @lives = loaded_state["lives"]
+                main_game_loop
+            else
+                pick_word
+                main_game_loop "No save exists. Starting a new game instead..."
+            end
         end
     end
 
@@ -69,32 +80,50 @@ class HangmanGame
         gets.chomp.downcase
     end
 
-    def main_game_loop
-        pick_word
-
+    def main_game_loop msg=nil
         loop do
 
             system "clear"
 
             puts AsciiArt::HANGMAN_LOGO
 
-            puts "Pssst! word is \"#{@choosen_word}\""
+            if msg
+                print msg + "\n\n"
+                msg = nil
+            end
 
             print_word_state
 
             if @lives < 1
                 print "\nGAME OVER!\n"
                 break
+            elsif !@word_state.include? "_"
+                print "\nYOU GUESSED IT RIGHT!\n"
+                puts "Good job!"
+                File.delete('./save.txt') unless !File.exist?('./save.txt')
+                break
             end
 
             user_input = read_user_input
 
+            @user_guesses += 1
+
             if user_input == "exit"
-                raise NotImplementedError
+                # must save and exit
+                #raise NotImplementedError
+                puts "Saving..."
+                
+                File.write('./save.txt', Marshal.dump({
+                    "lives"=> @lives,
+                    "choosen_word" => @choosen_word,
+                    "word_state" => @word_state
+                }))
+                
+                puts "Done. Goodbye!"
+                break
             else
                 letter = user_input[0]
                 process_letter letter
-
             end
         end
     end
